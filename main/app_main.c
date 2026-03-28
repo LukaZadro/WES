63eb5f5389fadb342184349ab6c6d5465b09e004
Luka Zadro <lukazadro987@gmail.com>
2026-03-28 16:38:35 +0100

    Dodao drivere za zvucnik i stavio test u main da pusta neki sound na kratko

--- main/app_main.c
+++ main/app_main.c
@@ -6,9 +6,13 @@
 * COPYRIGHT NOTICE: (c) 2022 Byte Lab Grupa d.o.o.
 * All rights reserved.
 */
-
 //--------------------------------- INCLUDES ----------------------------------
 #include "gui.h"
+#include "max98357a.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
 //---------------------------------- MACROS -----------------------------------
 
 //-------------------------------- DATA TYPES ---------------------------------
@@ -16,12 +20,37 @@
 //---------------------- PRIVATE FUNCTION PROTOTYPES --------------------------
 
 //------------------------- STATIC DATA & CONSTANTS ---------------------------
+static const char *TAG = "app_main";
 
 //------------------------------- GLOBAL DATA ---------------------------------
 
 //------------------------------ PUBLIC FUNCTIONS -----------------------------
+static void _sound_task(void *arg)
+{
+   max98357a_cfg_t cfg = {
+      .i2s_num         = 0,
+      .gpio_bclk       = 26,   /* change to your wired GPIO */
+      .gpio_lrc        = 25,   /* change to your wired GPIO */
+      .gpio_dout       = 27,   /* change to your wired GPIO */
+      .gpio_sd_mode    = -1,   /* -1 if SHDN pin is tied HIGH */
+      .sample_rate     = 44100,
+      .bits_per_sample = 16,
+   };
+   ESP_LOGI(TAG, "Initialising amplifier...");
+   max98357a_init(&cfg);
+
+   max98357a_play_tone(523, 320, 80);  /* C5 */
+   max98357a_play_tone(659, 320, 80);  /* E5 */
+   max98357a_play_tone(784, 500, 80);  /* G5 */
+   ESP_LOGI(TAG, "Done.");
+
+   max98357a_deinit();
+   vTaskDelete(NULL);
+}
+
 void app_main(void)
 {
+   xTaskCreatePinnedToCore(_sound_task, "sound", 4096, NULL, 5, NULL, 0);
    gui_init();
 }
 
